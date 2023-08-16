28c604e9c018b5344273d2f5ec3d7bb5454f9c55
Philip Thomsen <philip-th@live.dk>
2023-08-16 09:26:54 +0200

    Card
    setup teh card component with a destructered prop and added a test object to App.jsx

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { CardStyle } from './Card.style'
 
-function Card() {
-  return (
-    <CardStyle>
-        <div>Card</div>
-    </CardStyle>
-  )
-}
+const Card = ({ product }) => {
+    return (
+        <CardStyle>
+                 <div className="card">
+        <img src={product.thumbnail} alt={product.title} className="card-image" />
+        <h3 className="card-title">{product.title}</h3>
+        <p className="card-description">{product.description}</p>
+        <p className="card-price">${product.price}</p>
+        <p className="card-discount">{product.discountPercentage}% off</p>
+        <div className="card-rating">Rating: {product.rating}</div>
+        <p className="card-stock">Stock: {product.stock} left</p>
+        <p className="card-brand">Brand: {product.brand}</p>
+        <p className="card-category">Category: {product.category}</p>
+      </div> 
+        </CardStyle>
 
+    );
+  };
 export default Card
--- src/Components/Card/Card.style.js
+++ src/Components/Card/Card.style.js
@@ -1,5 +1,54 @@
 import styled from "styled-components";
 
 export const CardStyle = styled.section`
+.card {
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  padding: 16px;
+  margin: 16px;
+  width: 300px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.card-image {
+  width: 100%;
+  max-height: 200px;
+  object-fit: cover;
+  border-radius: 8px;
+}
+
+.card-title {
+  font-size: 1.2rem;
+  margin-top: 8px;
+}
+
+.card-description {
+  margin-top: 8px;
+}
+
+.card-price {
+  font-weight: bold;
+  margin-top: 8px;
+}
+
+.card-discount {
+  color: #e74c3c;
+  margin-top: 4px;
+}
+
+.card-rating {
+  margin-top: 4px;
+}
+
+.card-stock {
+  margin-top: 4px;
+}
+
+.card-brand,
+.card-category {
+  margin-top: 8px;
+  font-style: italic;
+  color: #555;
+}
 
 `
